8e420ca99200c293fe542d8be93a0eff5ab6caf3
absawye2 <absawye2@ncsu.edu>
2026-08-18 11:14:06 -0400

    [Comments] fixing up doc strings]

--- fbi.py
+++ fbi.py
@@ -8,21 +8,28 @@ class FBI:
     full of the information that is held in the fbis top 10 most wanted murderers list
     """
     def __init__(self):
+        """
+        This initalizes the single value that is constant in this class.
+        This value is the fbiDict which starts empty but will later come to contain all of the data referring to the
+        FBI's most wanted list
+        """
         self.fbidict = {}
 
-    """
-    This makes the dict that is filled with api data
-    :return: dict
-    """
+
     def makeDict(self):
+        """
+        This makes the dict that is filled with api data
+        :return: dict
+        """
         self.fbidict = self.grabAPI()
         return self.fbidict
 
-    """
-    gets and opens the api and retrieves data to be put into a dict with their name and crime
-    :return: dict
-    """
+
     def grabAPI(self):
+        """
+        gets and opens the api and retrieves data to be put into a dict with their name and crime
+        :return: dict
+        """
         people = {}
         page = 1
 
@@ -42,6 +49,10 @@ class FBI:
         return people
 
 def main():
+    """
+    This is the main function for this specified file, It is used to ensure the file is working properly
+    via pytest
+    """
     fbi = FBI()
     fbi.makeDict()
     print(len(fbi.fbidict))
