a12ad8f5f034a3725a9d77403f8b4dc91e2ba6f7
absawye2 <absawye2@ncsu.edu>
2026-08-18 13:37:56 -0400

    [implementation] fixing API implementation

--- spotify.py
+++ spotify.py
@@ -1,8 +1,6 @@
 import requests
 import sys
 
-CLIENT_ID = "105adc1917f7412c9eed0f569495de52"
-CLIENT_SECRET = "e8600589047d4becaaa7a05205f8e0ef"
 PERSON_NAME = "Robert William Fisher"
 
 url = "https://api.spotify.com/v1/search"
@@ -20,7 +18,7 @@ ACCESS_TOKEN = auth_response.json()["access_token"]
 headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
 params = {
     "q": f'"{PERSON_NAME}"',
-    "type": "episode",
+    "type": "show",
     "market": "US",
     "limit": 50,
 }
