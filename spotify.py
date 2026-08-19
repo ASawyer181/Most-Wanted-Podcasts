d87acdad5a6546bf401396a7277436a9af54cb62
absawye2 <absawye2@ncsu.edu>
2026-08-19 14:42:01 -0400

    [implementation] trying to get a spotify API key

--- spotify.py
+++ spotify.py
@@ -1,5 +1,13 @@
 import requests
+from dotenv import load_dotenv
+import os
 import sys
+
+load_dotenv() 
+
+CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+
 
 PERSON_NAME = "Robert William Fisher"
 
